c45e502ea209bac23181962f63cba2a1e745cfb5
artium_96 <artium20@gmail.com>
2023-01-21 20:44:13 +0200

    fix bag dark mode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -2,16 +2,13 @@ import "../css/DarkMode.css";
 import React from 'react'
 
 
-//here component dark mode we can show in all pages in side - we use this component in Menu component
-
 function DarkMode() {
 
+  const storedTheme = localStorage.getItem("theme");
 
-  // 1
+
   const setDark = () => {
-    // 2
     localStorage.setItem("theme", "dark");
-    // 3
     document.documentElement.setAttribute("data-theme", "dark");
   };
 
@@ -23,19 +20,9 @@ function DarkMode() {
   };
 
 
+  const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-  // 4
-  const storedTheme = localStorage.getItem("theme");
-
-
-  const prefersDark =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-
-
-  const defaultDark =
-    storedTheme === "dark" || (storedTheme === null && prefersDark);
+  const defaultDark = storedTheme === "dark" || (storedTheme === null && prefersDark);
 
 
   if (defaultDark) {
@@ -44,10 +31,9 @@ function DarkMode() {
 
 
 
-  // 5
   const toggleTheme = (e) => {
 
-    window.location.reload(false); // רענון דף
+    window.location.reload(false);
 
     if (e.target.checked) {
       setDark();
@@ -55,7 +41,6 @@ function DarkMode() {
       setLight();
     }
   };
-
 
 
 
@@ -67,7 +52,6 @@ function DarkMode() {
         <input
           type="checkbox"
           id="checkbox"
-          // 6
           onChange={toggleTheme}
           defaultChecked={defaultDark}
         />
@@ -80,92 +64,6 @@ function DarkMode() {
 };
 
 export default DarkMode;
-
-
-
-
-
-
-
-
-
-// import "../css/DarkMode.css";
-// import React, { useEffect } from 'react'
-
-
-// //here component dark mode we can show in all pages in side - we use this component in Menu component
-// function DarkMode() {
-
-
-//   const storedTheme = localStorage.getItem("theme");
-
-//   const defaultDark = storedTheme === "dark" || storedTheme === null;
-
-
-
-//   const setDark = () => {
-
-//     localStorage.setItem("theme", "dark");
-//     document.documentElement.setAttribute("data-theme", "dark");
-//   };
-
-
-
-//   const setLight = () => {
-
-//     localStorage.setItem("theme", "light");
-//     document.documentElement.setAttribute("data-theme", "light");
-//   };
-
-
-
-//   const toggleTheme = (e) => {
-
-//     window.location.reload(false);
-
-//     if (e.target.checked) {
-//       setDark();
-//     }
-//     else {
-//       setLight();
-//     }
-
-//   };
-
-
-
-
-//   useEffect(() => {
-
-//     if (defaultDark) {
-//       setDark();
-//     }
-//   });
-
-
-
-
-//   return (
-
-//     <div className="toggle-theme-wrapper">
-//       <span>🌒</span>
-//       <label className="toggle-theme" htmlFor="checkbox">
-//         <input
-//           type="checkbox"
-//           id="checkbox"
-//           onChange={toggleTheme}
-//           defaultChecked={defaultDark}
-//         />
-//         <div className="slider round"></div>
-//       </label>
-//       <span>☀️</span>
-//     </div>
-//   );
-
-// };
-
-// export default DarkMode;
-
 
 
 
