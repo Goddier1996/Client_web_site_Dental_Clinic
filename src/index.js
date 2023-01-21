7829adf97b83db3648af0d2495bd2432ab730efd
artium_96 <artium20@gmail.com>
2023-01-21 21:46:57 +0200

    remove

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
 
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
 
-  </React.StrictMode>,
 
 );
