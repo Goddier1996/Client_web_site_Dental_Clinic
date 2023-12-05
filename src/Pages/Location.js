97ce519eac3a463666fe20c1c01af0ef9f15bf4d
artium_96 <artium20@gmail.com>
2023-12-05 18:44:38 +0200

    create component Distance in contact page

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -8,6 +8,7 @@ import { container } from "../styleComponents/StyleAnimation"
 import ContactInfo from '../components/locationContact/ContactInfo';
 import ShowHoursWork from '../components/locationContact/ShowHoursWork';
 import ButtonSendEmail from '../components/locationContact/ButtonSendEmail';
+import CalcDistanceToClinic from '../components/locationContact/CalcDistanceToClinic';
 
 
 
@@ -53,7 +54,11 @@ function Location() {
                         <SendEmail hideModelSendMessage={closePopUpModelSendMessage} />
                     </Modal>
 
-                    
+
+                    {/* here show distance go to clinic from user location */}
+                    <CalcDistanceToClinic />
+
+
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6743.47603500594!2d34.9265383734192!3d32.31889140639387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d15e9603fb971%3A0x6ec59fbe65208f34!2sThe%20Village%20Mall!5e0!3m2!1sen!2sil!4v1659106270835!5m2!1sen!2sil"
                         width="100%" height="280" style={{ border: "0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
                     </iframe>
---
+++ src/components/locationContact/CalcDistanceToClinic.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+
+const CalcDistanceToClinic = () => {
+
+    const haversine = require('haversine')
+
+    const [distance, setDistance] = useState("")
+
+    let storedTheme = localStorage.getItem("theme");
+
+    const end = {
+        latitude: 32.31822,
+        longitude: 34.93413
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+
+            setDistance(haversine(position.coords, end, { unit: 'km' }))
+        },
+        () => {
+            console.log("Position could not be determined.")
+        }
+    );
+
+
+    return (
+        <div className='qq'>
+            <p>
+                <span className={(storedTheme == "light") ? "boldFirstWordDark" : (storedTheme == "dark") ? "boldFirstWord" : ""}>
+                    Distance to the clinic from your place : </span>
+                {distance == Math.floor(distance) || distance >= 1 ?
+                    <>{parseFloat(distance).toFixed(0)} Km</>
+                    :
+                    distance < 1 ?
+                        <>{parseFloat(distance).toFixed(3)} Meters</>
+                        :
+                        !distance ?
+                            <><br />Your location data was not received,<br />please share your location.</> : ""
+                }
+            </p>
+        </div>
+    )
+}
+
+export default CalcDistanceToClinic
