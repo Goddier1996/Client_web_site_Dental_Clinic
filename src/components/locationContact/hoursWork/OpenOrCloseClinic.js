9253bdf41dd4a713a95ef4816d738255e468ee69
artium_96 <artium20@gmail.com>
2023-12-10 09:53:01 +0200

    clear free space code

--- src/components/locationContact/hoursWork/OpenOrCloseClinic.js
+++ src/components/locationContact/hoursWork/OpenOrCloseClinic.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { GetDayWeekFromArray,GetTime } from "../../addAppointment/AlertUserHaveTurnToday"
+import { GetDayWeekFromArray, GetTime } from "../../addAppointment/AlertUserHaveTurnToday"
 
 
 const OpenOrCloseClinic = () => {
