6beaeabd06a2efb9db177efafadc0e9d6d1062b9
artium_96 <artium20@gmail.com>
2024-02-02 20:49:58 +0200

    add title at ShowDayAndHourSelectUser component about turn user select

--- src/components/addAppointment/saveTurnShowPopUp/ShowDayAndHourSelectUser.jsx
+++ src/components/addAppointment/saveTurnShowPopUp/ShowDayAndHourSelectUser.jsx
@@ -5,6 +5,7 @@ const ShowDayAndHourSelectUser = ({ day, hour }) => {
 
   return (
     <div className="showInfoUserQueue">
+      <h6>Information about the queue I selected</h6>
       <i class="bi bi-arrow-down"></i>
       <p>
         Day: {day}
