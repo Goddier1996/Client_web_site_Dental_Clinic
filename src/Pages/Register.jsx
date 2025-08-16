419b297899c2102d227fb2bc0141703f0f385140
artium_96 <artium20@gmail.com>
2025-08-16 19:13:58 +0300

    add filter blur to main img register page, because the img size was a very big

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
-import '../css/register.css'
-import { motion as m } from "framer-motion/dist/framer-motion"
-import UserRegister from '../components/register/UserRegister.jsx';
-
+import React, { useEffect, useState } from "react";
+import "../css/register.css";
+import { motion as m } from "framer-motion/dist/framer-motion";
+import UserRegister from "../components/register/UserRegister.jsx";
 
 function Register() {
 
-    let storedTheme = localStorage.getItem("theme");
 
-    return (
-        <m.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.75, ease: "easeOut" }}
+  let storedTheme = localStorage.getItem("theme");
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = "https://i.postimg.cc/DzMNdwky/55.jpg";
+    img.onload = () => setLoaded(true);
+  }, []);
+
+    
+  return (
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+    >
+      <section className="banner1">
+        <div
+          className={
+            storedTheme == "light"
+              ? "box1 contect1Dark"
+              : storedTheme == "dark"
+              ? "box1 contect1"
+              : ""
+          }
         >
-            <section className="banner1">
-                <div className={(storedTheme == "light") ? "box1 contect1Dark" : (storedTheme == "dark") ? "box1 contect1" : ""}>
-                    <div className={(storedTheme == "light") ? "log1Dark" : (storedTheme == "dark") ? "log1" : ""}>
-                        <UserRegister />
-                    </div>
-                </div>
-                <div className="box1 image1"></div>
-            </section>
-        </m.div>
-    )
+          <div
+            className={
+              storedTheme == "light"
+                ? "log1Dark"
+                : storedTheme == "dark"
+                ? "log1"
+                : ""
+            }
+          >
+            <UserRegister />
+          </div>
+        </div>
+
+        {/* show main img register page */}
+        <div
+          className="box1 image1"
+          style={{
+            backgroundImage: loaded
+                  ? `url(https://i.postimg.cc/DzMNdwky/55.jpg)` :
+               ` url(https://i.postimg.cc/027GcQD2/main-home-img.webp)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            filter: loaded ? "none" : "blur(20px)",
+            transition: "filter 0.6s ease-out",
+          }}
+        ></div>
+      </section>
+    </m.div>
+  );
 }
 
 
